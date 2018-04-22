e6db865afac778be73a646796de25083fce3dcbd
Ivan <khyzhniak.i@outlook.com>
2018-04-22 15:55:01 +0000

    shows component is ready, but I'd change the model to TIME and SORT BY TIME

--- app/hyperloop/components/edit_item.rb
+++ app/hyperloop/components/edit_item.rb
@@ -6,14 +6,14 @@ class EditItem < Hyperloop::Component
   param :className
   after_mount { Element[dom_node].focus }  # add
 
+  
   render do
     INPUT(
       type: :time,
       class: params.className, 
       defaultValue: params.todo.title,
-      key: params.todo.object_id,
-      placeholder: "What is left to do today?",
-      style: {"padding-left": 16, width: "min-content"}
+      style: {"padding-left": 16},
+      key: params.todo.id
     ).on(:key_down) do |evt|
       next unless evt.key_code == 13
       params.todo.update(title: evt.target.value)
